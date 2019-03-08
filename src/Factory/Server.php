e4717175028913f32c8bcbfd78130e87908e3618
xcalder <xcalder@foxmail.com>
2019-03-08 18:10:55 +0800

    00

--- src/Factory/Server.php
+++ src/Factory/Server.php
@@ -21,6 +21,10 @@ class Server
     
     private $array_config = [];
     
+    private $config_tag_type = [];
+    
+    private $config_rule_text = [];
+    
     /**
      * 取未开始/进行中的活动
      */
@@ -53,11 +57,11 @@ class Server
      */
     public function getRuningProducts($product_ids, $product_specification_value_to_product_ids){
         if(empty($this->products)){
-            return $this->products;
+            //return $this->products;
         }
         $this->products = ProductActivityRuleProducts::join('product_activity as pa', function($join){
             $join->on('pa.id', '=', 'product_activity_rule_products.activity_id');
-        })->where('product_activity_rule_products.rule_product_type', 1)->whereIn('product_activity_rule_products.product_id', $product_ids)->whereIn('product_activity_rule_products.product_specification_value_to_product_id', $product_specification_value_to_product_ids)->where('product_activity_rule_products.status', 6)->select(['pa.id', 'product_activity_rule_products.activity_id', 'pa.tag', 'pa.tag_img','product_activity_rule_products.product_id'])->get()->toArray();
+        })->where('product_activity_rule_products.rule_product_type', 1)->whereIn('product_activity_rule_products.product_id', $product_ids)->whereIn('product_activity_rule_products.product_specification_value_to_product_id', $product_specification_value_to_product_ids)->where('product_activity_rule_products.status', 6)->select(['pa.id', 'product_activity_rule_products.activity_id', 'pa.tag', 'pa.type', 'pa.tag_img','product_activity_rule_products.product_id'])->get()->toArray();
         //$this->activity_ids = lumen_array_column($this->products, 'activity_id');
         return $this->products;
     }
@@ -110,6 +114,11 @@ class Server
             }
             return $request;
         }
+        
+        $this->getActivityConfig(config('all_status.activity'));
+        foreach ($products as $key=>$value){
+            $products[$key]['tag_type'] = $this->config_tag_type[$value['type']] ?? '';
+        }
         $products = array_under_reset($products, 'product_id', 2);
         if(!empty($products)){
             foreach ($products as $key=>$value){
@@ -126,18 +135,28 @@ class Server
         if(empty($request)){
             return $request;
         }
-        $activitys =  ProductActivityRuleProducts::where('product_id', $request['id'])->where('status', 6)->where('rule_product_type', 1)->get();
+        $activitys =  ProductActivityRuleProducts::join('product_activity_rules as par', function($join){
+            $join->on('par.id', '=', 'product_activity_rule_products.activity_rules_id');
+        })->where('product_activity_rule_products.product_id', $request['id'])->where('product_activity_rule_products.status', 6)->where('product_activity_rule_products.rule_product_type', 1)->get()->toArray();
+        if(!empty($activitys)){
+            $this->getActivityConfig(config('all_status.activity'));
+            foreach ($activitys as $key=>$value){
+                //$activitys[$key]['rule_text'] = sprintf($this->config_rule_text[$value['type']], $value['total']);
+            }
+        }
         $request['activitys'] = $activitys;
         return $request;
     }
     
-    private function getActivityNames($activity_config){
+    private function getActivityConfig($activity_config){
         if(!empty($activity_config)){
             foreach ($activity_config as $key=>$value){
                 if(is_array($value) && !isset($value['name'])){
-                    $this->getActivityNames($value);
+                    $this->getActivityConfig($value);
                 }else{
-                    $this->array_config[$value['type']] = $value['name'];
+                    $this->config_tag_type[$value['type']] = $value['tag_type'] ?? '';
+                    $this->array_config[$value['type']] = $value['name'] ?? '';
+                    $this->config_rule_text[$value['type']] = $value['rule_text'] ?? '';
                 }
             }
         }
